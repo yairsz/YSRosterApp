744a8d7d439eb17133b9dcdc40d545c18f1bc97f
Yair Szarf <2handed@gmail.com>
2014-01-13 12:52:34 -0800

    Initial Commit

---
+++ YSRosterApp/YSViewController.h
@@ -0,0 +1,13 @@
+//
+//  YSViewController.h
+//  YSRosterApp
+//
+//  Created by Yair Szarf on 1/13/14.
+//  Copyright (c) 2014 The 2 Handed Consortium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YSViewController : UIViewController
+
+@end
